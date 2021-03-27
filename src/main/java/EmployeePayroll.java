c2abe09df40b361ee1977dc179052bf03a02cc64
KrishnaNimmanagoti <krishna32226@gmail.com>
2021-03-27 21:17:53 +0530

    Refactored to retrive the Employee Details from the database

--- src/main/java/EmployeePayroll.java
+++ src/main/java/EmployeePayroll.java
@@ -1,13 +1,16 @@
 import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.List;
 
 public class EmployeePayroll {
     private static final String URL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
     private static final String user = "root";
     private static final String password = "krishna";
 
-    private void establishConnection() {
-        Connection connection;
+    private Connection establishConnection() {
+        Connection connection = null;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Driver found!");
@@ -22,6 +25,7 @@ public class EmployeePayroll {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return connection;
     }
 
     private void listDrivers() {
@@ -30,6 +34,25 @@ public class EmployeePayroll {
             Driver driverClass = driverList.nextElement();
             System.out.println("Driver: " + driverClass.getClass().getName());
         }
+    }
+
+    public List<EmployeePayrollData> readData() {
+        String sql = "SELECT * FROM employee_details;";
+        List<EmployeePayrollData> employeePayrollData = new ArrayList<>();
+        try (Connection connection = this.establishConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String gender = resultSet.getString("gender");
+                LocalDate startDate = resultSet.getDate("start").toLocalDate();
+                employeePayrollData.add(new EmployeePayrollData(id, name, gender, startDate));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employeePayrollData;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/EmployeePayrollData.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+    public int id;
+    public String name;
+    public String gender;
+    public LocalDate start;
+
+    public EmployeePayrollData(int id, String name, String gender, LocalDate start) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.start = start;
+    }
+}
